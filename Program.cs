0764bcee1034157f1249f1052bfee1707ecaabe1
pietro zanaga <pietrozneto@gmail.com>
2023-12-27 15:38:21 -0300

    Implementação das classes de smartphones

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero : "135791357", modelo: "Tijolo", imei : "11222", memoria: 256);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Clash of clans");
+
+Console.WriteLine("\n");
+
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero : "2135791357", modelo: "35promax", imei : "4444444", memoria: 2048);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Clahs royale");
